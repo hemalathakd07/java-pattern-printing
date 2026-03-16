f565d05b16759c6501c4985aecdc7f0071d99c4d
hemalathakd07 <hemalathakd07@gmail.com>
2026-03-16 20:57:08 +0530

    Add right triangle pattern in java

---
+++ rightTrianglePattern.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class rightTrianglePattern {
+    public static void main(String[] args) {
+        int rows;
+        Scanner scanner = new Scanner(System.in) 
+          System.out.println("Enter number of rows:");
+          rows=scanner.nextInt();
+        
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
