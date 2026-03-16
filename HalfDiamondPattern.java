74f80600d7845af44ee479adb9349fb1da9588d3
hemalathakd07 <hemalathakd07@gmail.com>
2026-03-16 21:09:08 +0530

    Changed file name

---
+++ HalfDiamondPattern.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class HalfDiamondPattern {
+    public static void main(String[] args) {
+        System.out.println("Enter number of rows:");
+        Scanner scanner=new Scanner(System.in);
+        int rows=scanner.nextInt();
+
+        for(int i=1;i<=rows;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i=rows-1;i>0;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
