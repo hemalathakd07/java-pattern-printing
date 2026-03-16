cad086ecd2064f92bfc7289db0699761eb6bfd72
hemalathakd07 <hemalathakd07@gmail.com>
2026-03-16 21:07:23 +0530

    Add inverted pyramid pattern

---
+++ invertedPyramidPattern.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class invertedPyramidPattern {
+  public static void main(String[] args) {
+    System.out.println("Enter number of rows:");
+        Scanner scanner=new Scanner(System.in);
+        int rows=scanner.nextInt();
+
+        for(int i=rows;i>0;i--){
+            for(int j=1;j<=rows-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=2*i-1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+  }
+}
