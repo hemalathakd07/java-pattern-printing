32acc4354fc5014dbe0aebc443ff2e51a1f717ac
hemalathakd07 <hemalathakd07@gmail.com>
2026-03-18 09:25:51 +0530

    Added right aligned half pyramid pattern

---
+++ rightAlignedHalfDaimondPattern.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class rightAlignedHalfDaimondPattern {
+    public static void main(String[] args) {
+        System.out.println("Enter number of rows:");
+        Scanner scanner=new Scanner(System.in);
+        int rows=scanner.nextInt();
+        for(int i=1;i<=rows;i++){
+            for(int j=1;j<=rows-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i=rows-1;i>0;i--){
+            for(int j=1;j<=rows-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
