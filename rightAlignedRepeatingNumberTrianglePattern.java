3dd930c4fe574f5c9c8e8b6cee9f53b591ffbc11
hemalathakd07 <hemalathakd07@gmail.com>
2026-03-20 10:16:02 +0530

    Added right aligned repeating number triangle pattern

---
+++ rightAlignedRepeatingNumberTrianglePattern.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class rightAlignedRepeatingNumberTrianglePattern {
+    public static void main(String[] args) {
+        System.out.println("Enter number of rows:");
+        Scanner scanner=new Scanner(System.in);
+        int rows=scanner.nextInt();
+        for(int i=1;i<=rows;i++){
+            for(int j=1;j<=rows-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
