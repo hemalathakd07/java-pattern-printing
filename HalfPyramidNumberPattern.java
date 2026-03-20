76e09cc347166a20e15b21923f06e6e36efa58b7
hemalathakd07 <hemalathakd07@gmail.com>
2026-03-20 10:13:07 +0530

    Added half pyramid number pattern

---
+++ HalfPyramidNumberPattern.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class HalfPyramidNumberPattern {
+    public static void main(String[] args) {
+        System.out.println("Enter number of rows:");
+        Scanner scanner=new Scanner(System.in);
+        int rows=scanner.nextInt();
+
+        for(int i=1;i<=rows;i++){
+            for(int j=1;j<=rows-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print(k+" ");
+            }
+            System.out.println();
+        }
+    }
+}
