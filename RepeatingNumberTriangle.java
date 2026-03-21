a495c5352de8aefd13896970d3935c414d0b8b59
hemalathakd07 <hemalathakd07@gmail.com>
2026-03-21 10:20:21 +0530

    Added repeating number triangle pattern

---
+++ RepeatingNumberTriangle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class RepeatingNumberTriangle {
+   public static void main(String[] args) {
+        System.out.println("Enter number of rows:");
+        Scanner scanner=new Scanner(System.in);
+        int rows=scanner.nextInt();
+
+        for(int i=1;i<=rows;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
