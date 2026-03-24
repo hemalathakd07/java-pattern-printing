de148ff55764751b54bdad0ec69b7694e19cfbd7
hemalathakd07 <hemalathakd07@gmail.com>
2026-03-24 10:11:48 +0530

    Created a new folder and organised files

--- alphabet-pattern/AlphabetHalfPyramid.java
+++ alphabet-pattern/AlphabetHalfPyramid.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
 
-public class AlphabetContinuousTriangle {
+public class AlphabetHalfPyramid {
   public static void main(String[] args) {
         System.out.println("Enter number of rows:");
         Scanner scanner=new Scanner(System.in);
         int rows=scanner.nextInt();
-        char ch='A';
         for(int i=1;i<=rows;i++){
-            for(int j=1;j<=i;j++){
+            char ch='A';
+            for(int j=1;j<=rows-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
                 System.out.print(ch+" ");
                 ch++;
             }
             System.out.println();
-
         }
     }
 }
