9d0a8ce07a3179f139d9302dc2681e1b4806b5e7
hemalathakd07 <hemalathakd07@gmail.com>
2026-03-24 10:16:04 +0530

    created a new folder and organised files

---
+++ numbers-pattern/FloydsTrianglePattern.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class FloydsTrianglePattern {
+    public static void main(String[] args) {
+        System.out.println("Enter number of rows:");
+        Scanner scanner=new Scanner(System.in);
+        int rows=scanner.nextInt();
+        int num=1;
+        for(int i=1;i<=rows;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(num+" ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}
